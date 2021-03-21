c4b9a50010fc8afb9b07f99332986fc4025b7ddc
Seth M Cohen <smcohen82@gmail.com>
2021-03-20 23:19:33 -0400

    done.

--- app/models/duck.rb
+++ app/models/duck.rb
@@ -1,2 +1,9 @@
 class Duck < ApplicationRecord
+  belongs_to :student, optional: true
+  validates :name, presence: true
+  validates :description, presence: true
+
+  def self.studentless_ducks
+    Duck.where(student_id: nil)
+  end
 end
