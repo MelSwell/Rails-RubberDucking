0d854dbbeb911a2232a59435ac2885ce80c980d5
Seth M Cohen <smcohen82@gmail.com>
2021-03-20 23:18:50 -0400

    commit tree 796b0e2d890987d06b802f87ed94a396e2b6c245

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,52 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+    if @student.ducks.any?
+      @ducks = @student.ducks
+    end
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+
+    if @student.valid?
+      redirect_to student_path(@student)
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to new_student_path
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+
+    if @student.update(student_params)
+      redirect_to student_path(@student)
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to edit_student_path
+    end
+  end
+
+  def destroy
+    Student.find_by_id(params[:id]).destroy
+    redirect_to students_path
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
+  end
+
 end
